8efe7cb17b05379434a1ac625c33e6949c48d9c7
Paul <paulasquith150@gmail.com>
2024-01-16 23:37:00 +0000

    Added Screenshot and readme file

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Quiz-Game
+# WK6HWK Quiz Game - Readme Webpage
+## Description
+
+The repository will meet the requirements of the Week 6 homework for the Front line Developer course 
+
+## Installation
+
+N/A
+
+## Usage
+
+The quiz asks questions relating to coding.
+
+![Screenshot of the app ](https://paul-codecourse.github.io/WK6HWK-Quiz-Game/assets/WK6_Quiz_Screenshot.jpg)
+
+## Credits
+
+N/A
+
+## License
+
+MIT Licence, please refer to the detailed explanation in the repository.
+---
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,9 +1,9 @@
 // Quiz Questions
 const questions = [
     {
-      question: "Who is the current head of the Anglican Church?",
-      options: ["Pope", "Dalai Lama", "King Charles", "Prince William"],
-      correctAnswer: "King Charles",
+      question: "JavaScript is ...?",
+      options: ["Written in coffee", "the written form of the Java programming language", "a programming language for the Internet", "stressfull"],
+      correctAnswer: "a programming language for the Internet",
     },
     {
       question: "What does the acronym HTML stand for?",
@@ -16,10 +16,10 @@ const questions = [
       correctAnswer: "Linux",
     },
     {
-        question: "Who is 'the' Dr?",
-        options: ["Dr Who", "Dr No", "Dr Why", "Which Dr?"],
-        correctAnswer: "Dr Who",
-      },
+      question: "How do you declare an array in JavaScript?",
+      options: ["let theArray = []", "Well! I do declare My Array!", "let theArray = ()", "An array is a collection of data elements"],
+      correctAnswer: "let theArray = []",
+    },
   ];
   
   // Start the quiz
